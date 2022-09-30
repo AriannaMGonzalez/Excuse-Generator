b2cde2d6595027bc3905d5d4fa86bc7b4f398b95
AriannaMGonzalez <Jebuschan@live.com>
2022-09-30 16:00:18 +0000

    Initial Commit

--- src/app.js
+++ src/app.js
@@ -4,8 +4,27 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { left } from "@popperjs/core";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+function wordpicker(array) {
+  return array[Math.floor(Math.random() * whos.length)];
+}
+let whos = ["The dog", "My grandma", "His turtle", "My bird"];
+let actions = ["ate", "peed", "crushed", "broke"];
+let whats = ["my homework", "the keys", "the car"];
+let whens = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+window.onload = function getRandomInt(max) {
+  document.getElementById("excuse").innerHTML =
+    wordpicker(whos) +
+    wordpicker(actions) +
+    wordpicker(whats) +
+    wordpicker(whens);
+  console.log();
 };
